c4de7b0c3a1d8d6cc0cf3f8ea0b7a5718599e5fa
agus7in <agufunes@gmail.com>
2025-02-21 11:16:16 +0100

    Validations

--- UserManagementAPI/Program.cs
+++ UserManagementAPI/Program.cs
@@ -2,16 +2,20 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using FluentValidation;
+using FluentValidation.AspNetCore;
 using System.Collections.Generic;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddSingleton<IUserRepository, UserRepository>();
+builder.Services.AddValidatorsFromAssemblyContaining<UserValidator>();
+builder.Services.AddFluentValidationAutoValidation();
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Root");
 
 app.MapGet("/users", (IUserRepository repo) => repo.GetAllUsers());
 
@@ -21,16 +25,27 @@ app.MapGet("/users/{id}", (int id, IUserRepository repo) =>
     return user is not null ? Results.Ok(user) : Results.NotFound();
 });
 
-app.MapPost("/users", (User user, IUserRepository repo) => 
+app.MapPost("/users", async (User user, IUserRepository repo, IValidator<User> validator) => 
 {
+    var validationResult = await validator.ValidateAsync(user);
+    if (!validationResult.IsValid)
+    {
+        return Results.BadRequest(validationResult.Errors);
+    }
     repo.AddUser(user);
     return Results.Created($"/users/{user.Id}", user);
 });
 
-app.MapPut("/users/{id}", (int id, User updatedUser, IUserRepository repo) => 
+app.MapPut("/users/{id}", async (int id, User updatedUser, IUserRepository repo, IValidator<User> validator) => 
 {
     var user = repo.GetUserById(id);
     if (user is null) return Results.NotFound();
+
+    var validationResult = await validator.ValidateAsync(updatedUser);
+    if (!validationResult.IsValid)
+    {
+        return Results.BadRequest(validationResult.Errors);
+    }
 
     repo.UpdateUser(id, updatedUser);
     return Results.NoContent();
@@ -40,7 +55,6 @@ app.MapDelete("/users/{id}", (int id, IUserRepository repo) =>
 {
     var user = repo.GetUserById(id);
     if (user is null) return Results.NotFound();
-
     repo.DeleteUser(id);
     return Results.NoContent();
 });
@@ -78,4 +92,13 @@ public class UserRepository : IUserRepository
     }
 
     public void DeleteUser(int id) => _users.RemoveAll(user => user.Id == id);
+}
+
+public class UserValidator : AbstractValidator<User>
+{
+    public UserValidator()
+    {
+        RuleFor(user => user.Name).NotEmpty().WithMessage("Name is required.");
+        RuleFor(user => user.Email).NotEmpty().EmailAddress().WithMessage("A valid email is required.");
+    }
 }
--- UserManagementAPI/obj/Debug/net8.0/UserManagementAPI.AssemblyInfo.cs
+++ UserManagementAPI/obj/Debug/net8.0/UserManagementAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserManagementAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c87b81b566866322e0394e4bf41680d68f86e427")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserManagementAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserManagementAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
